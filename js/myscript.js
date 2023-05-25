25f9d21a663e764fcbead6ecfffce5d1a439236b
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-25 17:03:20 +0200

    add the distance of date and calculate for variabl

--- js/myscript.js
+++ js/myscript.js
@@ -30,10 +30,37 @@
 //     console.log("Prova");
 // }, 1000)
 
+86400000
 
-//? Teorie in testa. Creare 4 array per ogni tipo, un array per il giorno che arriva fino ad 01, un array per l'ora che arriva a 24,  minuti e secondi a 60, e questi numeri grazie al setInterval devono decrescere e "resettarsi" ogni volta.
+// con l'oggetto new date() creo la data attuale e quella che mi serve per vedere quanti millisecondi ho 
+// oppure usare math.floor per ogni varriabile per calcolare il valore che mi serve
 
-let days = [0,1];
-let hours = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
-let minuts = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
-let seconds = [00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
+
+//? Imposto la distanza tra le due date 
+
+let distance = countDown - now;
+
+
+//? imposto alle variabili il calcolo dei giorni, ore, minuti e secondi
+
+
+let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//? INSERISCO LA DATA CHE MI SERVE
+
+let countDown = newDate("may 26, 2023, 09:30:00").getTime();
+
+//? INSERISCO I SECONDI CHE SCORRONO ALL'INDIETRO
+
+
+let interval = setInterval(function(){
+
+},1000)
+
+
+//? INSERISCO LA DATA ATTUALE CHE POI MODIFICHERò FINITO IL CODICE
+
+let now = newDate("may 25, 2023, 16:53:00").getTime();
